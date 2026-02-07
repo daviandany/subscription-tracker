6d016a2e2ebb186985c081ee18bfe370f40871c0
daviandany <andanydavi2@gmail.com>
2026-02-07 18:03:22 -0300

    jwt/middlewares

--- src/middlewares/index.js
+++ src/middlewares/index.js
@@ -1,1 +1,21 @@
-const elton = 'elton'
+import jwt from 'jsonwebtoken';
+
+module.exports = function(req, res, next){
+
+    const token = req.header('x-auth-token');
+
+    if (!token) {
+        return res.status(401).json({ msg: "Acesso negado. token não encontrado" })
+    }
+
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+        req.user = decoded.user;
+
+        next()
+    } catch (err) {
+        res.status(401).json({ msg: 'Token inválido.' })
+    }
+
+}
